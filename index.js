1aedc07824870e9b591f31d7b0a6c8f1c566e60c
Anchorite1001 <yangwu.1415@gmail.com>
2021-03-24 21:47:36 +1100

    oauth setup try

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 //there are many projects with several express app but in this one there's only one 
 const app = express();
+
+passport.use(new GoogleStrategy());
+
+//route handler
 app.get('/', (req, res) => {
     res.send({ hi:'there' });
 });
